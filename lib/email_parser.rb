1e714a199fa326c9ed0b9e321fd4eb056a4c4e35
Rosie Choi <rosiechoimanso@gmail.com>
2020-11-18 22:21:28 +0000

    Automatically backed up by Learn

--- lib/email_parser.rb
+++ lib/email_parser.rb
@@ -3,7 +3,7 @@
 # unique email addresses. The delimiters to support are commas (',')
 # or whitespace (' ').
 
-<<<<<<< HEAD
+
 class EmailAddressParser
   attr_accessor :emails
   def initialize(emails)
@@ -14,7 +14,3 @@ class EmailAddressParser
   end
   
 end
-=======
-Class EmailAddressParser()
-end
->>>>>>> c43387703a5d5b2bd70a909215a14b1b8a5e3fad
